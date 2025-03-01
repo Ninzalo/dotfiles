ebbdf67d51ffaec7944a6fac1bbf1d83b179febd
Ninzalo <k-artemiy@mail.ru>
2025-03-02 00:46:03 +0300

    feat(sh/symlinks): add apply_symlinks script

---
+++ apply_symlinks.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+DOTFILES_DIR=$(pwd)
+
+CONFIG_PKGS="
+alacritty
+ghostty
+nvim
+tmux
+gitmux
+"
+
+ROOT_PKGS="
+p10k
+zsh
+"
+
+mkdir -p "$HOME/.config"
+
+for pkg in $CONFIG_PKGS; do
+	echo "Linking $pkg into ~/.config..."
+	ln -sf "$DOTFILES_DIR/$pkg" "$HOME/.config/$pkg"
+done
+
+for pkg in $ROOT_PKGS; do
+    echo "Processing $pkg files:"
+
+    find "$DOTFILES_DIR/$pkg" -mindepth 1 -maxdepth 1 ! -name ".git" | while read -r file; do
+        filename=$(basename "$file")
+        target="$HOME/$filename"
+
+        echo "Linking $filename to home directory..."
+        ln -sf "$file" "$target"
+    done
+done
